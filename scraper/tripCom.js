e704312fc119127952b0ea579f2c69c81472a141
Yash Kumar <128486714+Hero-Alpha@users.noreply.github.com>
2025-07-11 12:53:27 +0530

    Update tripCom.js

--- scraper/tripCom.js
+++ scraper/tripCom.js
@@ -15,15 +15,13 @@ module.exports = async function fetchTripComPrice({ hotelId, checkIn, checkOut, 
     });
 
     const page = await browser.newPage();
-    console.log('page.waitForTimeout:', typeof page.waitForTimeout);
-
 
     // Anti-bot fingerprinting protection
     await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/114.0.0.0 Safari/537.36");
     await page.setExtraHTTPHeaders({ "Accept-Language": "en-US,en;q=0.9" });
 
     console.log(`--Navigating to URL : [${finalUrl}]--`);
-    await page.goto(finalUrl, { waitUntil: "networkidle2", timeout: 60000 });
+    await page.goto(finalUrl, { waitUntil: "networkidle2", timeout: 30000 });
 
     await new Promise(resolve => setTimeout(resolve, 3000));
 
